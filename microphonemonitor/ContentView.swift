45e7ab191db081c0f9f3eb03e0d041447e1ef6e6
DEGUCHI Tatsuya <t.deg84@gmail.com>
2024-04-16 08:46:11 +0900

    とりあえずビルド出来た

--- microphonemonitor/ContentView.swift
+++ microphonemonitor/ContentView.swift
@@ -1,24 +1,89 @@
-//
-//  ContentView.swift
-//  microphonemonitor
-//
-//  Created by Owner on 2024/04/16.
-//
-
 import SwiftUI
+import CoreAudio
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+class MicrophoneMonitor: ObservableObject {
+    private var deviceID: AudioDeviceID = 0
+    private var deviceAddedObserver: AudioObjectPropertyListenerProc = { (inObjectID, inNumberAddresses, inAddresses, inClientData) -> OSStatus in
+        let monitor = Unmanaged<MicrophoneMonitor>.fromOpaque(inClientData!).takeUnretainedValue()
+        monitor.updateCurrentMicrophone()
+        return noErr
+    }
+    
+    @Published var currentMicrophone: String = ""
+    @Published var history: [String] = []
+    
+    init() {
+        setupAudioDeviceMonitoring()
+    }
+    
+    private func setupAudioDeviceMonitoring() {
+        var address = AudioObjectPropertyAddress(
+            mSelector: kAudioHardwarePropertyDefaultInputDevice,
+            mScope: kAudioObjectPropertyScopeGlobal,
+            mElement: kAudioObjectPropertyElementMaster
+        )
+        
+        var deviceID = AudioDeviceID()
+        var size = UInt32(MemoryLayout<AudioDeviceID>.size)
+        let status = AudioObjectGetPropertyData(AudioObjectID(kAudioObjectSystemObject), &address, 0, nil, &size, &deviceID)
+        
+        if status == noErr {
+            self.deviceID = deviceID
+            updateCurrentMicrophone()
+            
+            address.mSelector = kAudioDevicePropertyDeviceIsAlive
+            let unretainedSelf = Unmanaged.passUnretained(self).toOpaque()
+            AudioObjectAddPropertyListener(deviceID, &address, deviceAddedObserver, unretainedSelf)
         }
-        .padding()
+    }
+    
+    private func updateCurrentMicrophone() {
+        var address = AudioObjectPropertyAddress(
+            mSelector: kAudioDevicePropertyDeviceNameCFString,
+            mScope: kAudioDevicePropertyScopeInput,
+            mElement: kAudioObjectPropertyElementMaster
+        )
+        
+        var name: CFString = "" as CFString
+        var size = UInt32(MemoryLayout<CFString>.size)
+        let status = AudioObjectGetPropertyData(deviceID, &address, 0, nil, &size, &name)
+        
+        if status == noErr {
+            let microphone = name as String
+            currentMicrophone = microphone
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+            let dateString = dateFormatter.string(from: Date())
+            
+            let historyItem = "[\(dateString)] マイクが切り替わりました: \(microphone)"
+            history.append(historyItem)
+            
+            showNotification(mic: microphone)
+        }
+    }
+    
+    private func showNotification(mic: String) {
+        let notification = NSUserNotification()
+        notification.title = "マイクが切り替わりました"
+        notification.informativeText = "新しいマイク: \(mic)"
+        notification.soundName = NSUserNotificationDefaultSoundName
+        
+        NSUserNotificationCenter.default.deliver(notification)
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View {
+    @StateObject private var micMonitor = MicrophoneMonitor()
+    
+    var body: some View {
+        VStack {
+            Text("現在のマイク: \(micMonitor.currentMicrophone)")
+                .padding()
+            
+            List(micMonitor.history, id: \.self) { item in
+                Text(item)
+            }
+        }
+    }
 }
