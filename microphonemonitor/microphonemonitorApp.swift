bbc3cb2929e62d5f054fc0cba32fb82c265436cc
DEGUCHI Tatsuya <t.deg84@gmail.com>
2024-04-16 08:26:17 +0900

    Initial Commit

---
+++ microphonemonitor/microphonemonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  microphonemonitorApp.swift
+//  microphonemonitor
+//
+//  Created by Owner on 2024/04/16.
+//
+
+import SwiftUI
+
+@main
+struct microphonemonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
